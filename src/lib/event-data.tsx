97f27f87d3c313e4eeef61115ab2be592ef3fcbd
Nitansh Shankar <167768158+BIJJUDAMA@users.noreply.github.com>
2025-11-15 09:48:19 +0530

    Added defang data

--- src/lib/event-data.tsx
+++ src/lib/event-data.tsx
@@ -38,7 +38,6 @@ export const eventDb: Record<string, Event> = {
     report:
       "The CS101 series of events ended with a bang that was the Hack101 event. Hack 101 was a 12-hour hackathon where participants flexed the skills they learnt in the workshops leading up to the event. The hackathon was completely open and participants built a wide variety of projects on a wide range of topics from 2D platformers to movie recommendation systems. The winners were awarded with cash prizes and certificates. ",
     mainImage: "/hack101.jpg",
-    sponsors: ["temp.jpg"],
     gallery: ["temp.jpg", "temp.jpg", "temp.jpg", "temp.jpg"],
     tags: ["#Hack101", "#CS101", "#ACM", "#Amrita"],
   },
@@ -121,8 +120,21 @@ export const eventDb: Record<string, Event> = {
     report:
       "The hands-on Git & GitHub foundations workshop held at ACM Amrita. Participants mastered repository management, version control workflows, branching strategies, and collaborative development practices. The workshop wrapped up with live PR demonstrations and comprehensive open source collaboration guidance.The workshop attracted more than students from various academic years.",
     mainImage: "/git workshop/git workshop.jpg",
-    stats: [{ value: 120, prefix: "Attended By Over ", suffix: " Students" }], 
+    stats: [{ value: 200, prefix: "Attended By Over ", suffix: " Students" }], 
     gallery: ["/git workshop/git workshop 2.jpg",],
     tags: ["#Git", "#GitHub", "#VersionControl", "#OpenSource", "#Collaboration"],
   },
+ 
+  "defang-io-hackathon": { //events/defang-io-hackathon is the route
+  name: "Defang.io Hackathon 2025",
+  details: "17th Sept, 2025 | Anugraha Hall-AB3",
+  report:
+    "The Defang.io Hackathon was officially launched as part of the ACM Bootstrap '25 activities, marking the beginning of a focused and technically engaging competition.The Defang.io Hackathon was officially launched as part of the ACM Bootstrap '25 activities, marking the beginning of a focused and technically engaging competition.These challenges covered a wide range of domains- AI-assisted systems, automation tools, web-based utilities, scalable cloud applications, performance-oriented solutions, and creative developer productivity tools",
+  mainImage: "/defang/defang.jpg", 
+  sponsors: ["/defang/sponsor.svg"],
+  stats: [{ value: 150, prefix: "Engaged Over ", suffix: " Participants" }],
+  gallery: ["/defang/defang 2.JPG", "/defang/defang 3.JPG", "/defang/defang 4.JPG", "/defang/defang 5.jpg"],
+  tags: ["#DefangHackathon", "#Hackathon", "#ACM", "#Tech", "#Innovation"]
+},
+
 };
